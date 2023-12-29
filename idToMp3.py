00f6714eeb7a7265627ec3c6f5956b0a53c0128c
declankra <declankramper@gmail.com>
2023-12-29 17:49:26 -0600

    changed absolute path and addded print statement

--- idToMp3.py
+++ idToMp3.py
@@ -5,7 +5,7 @@ def read_nfc_tag():
     print("inside read_nfc_tag()")
     try:
         # Run the Node.js script and wait for 2 seconds for NFC read, then timeout
-        result = subprocess.run(['node', 'readNfcTag.js'], capture_output=True, text=True, timeout=2)
+        result = subprocess.run(['node', '/home/pi/raspberryPiRecordPlayerSonos2/readNfcTag.js'], capture_output=True, text=True, timeout=2)
         if result.returncode == 0:
             print(result.stdout.strip())
             return result.stdout.strip()
--- nfc_to_spotify.py
+++ nfc_to_spotify.py
@@ -1,6 +1,6 @@
 import json
 
-def get_spotify_uri_from_tag(tag_id, json_file='vinylCollection.json'):
+def get_spotify_uri_from_tag(tag_id, json_file='/home/pi/raspberryPiRecordPlayerSonos2/vinylCollection.json'):
     with open(json_file, 'r') as file:
         vinyl_collection = json.load(file)
     print("searching for uri from vinyl collection")
@@ -8,4 +8,5 @@ def get_spotify_uri_from_tag(tag_id, json_file='vinylCollection.json'):
         if record['tag_id'] == tag_id:
             print("found tag id in records")
             return record['spotify_URI']
+    print("no tag found")
     return None
