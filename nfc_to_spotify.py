82f046799d103dbaa8fe1d24bb6feaffe9135aa9
declankra <declankramper@gmail.com>
2023-12-30 08:14:37 -0600

    removed print statements

--- nfc_to_spotify.py
+++ nfc_to_spotify.py
@@ -3,9 +3,9 @@ import json
 def get_spotify_uri_from_tag(tag_id, json_file='/home/pi/raspberryPiRecordPlayerSonos2/vinylCollection.json'):
     with open(json_file, 'r') as file:
         vinyl_collection = json.load(file)
-    print("searching for uri from vinyl collection")
+    ##print("searching for uri from vinyl collection")
     for record in vinyl_collection:
         if record['tag_id'] == tag_id:
-            print("found tag id in records")
+            ##print("found tag id in records")
             return record['spotify_URI']
     return None
